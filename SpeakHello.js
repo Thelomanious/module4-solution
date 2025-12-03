69618c0282c293ab9f595b7e1a16d69bf0d4c79e
Thelomanious <mcox@hayesgibson.com>
2025-12-03 10:50:22 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -18,12 +18,14 @@ var speakWord = "Hello";
 (function (window) {
   var speakWord = "Hello";
   var helloSpeaker = {};
+
   helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
-  }
-    window.helloSpeaker = helloSpeaker;
-  })(window);
+  };
 
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
